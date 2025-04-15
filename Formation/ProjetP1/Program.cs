a53f9dbacc5466b6fe1ba8b95c3ec897798bd4dd
YannLeCam <yannick.lecam1@gmail.com>
2025-04-15 15:55:47 +0200

    Relocalisation du projet correctement

---
+++ Formation/ProjetP1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Compte compte = new Compte(1, 1000);
+            Compte compte1 = new Compte(2);
+            Compte compte2 = new Compte(3, 500);
+
+            Transaction tran = new Transaction(1, 200, 1, 2);
+            Transaction tran1 = new Transaction(2, 500, 0, 2);
+
+        }
+    }
+}
--- Formation/ProjetP1/StatutTransaction.cs
+++ Formation/ProjetP1/StatutTransaction.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjetS2
+namespace ProjetP1
 {
     internal class StatutTransaction
     {
